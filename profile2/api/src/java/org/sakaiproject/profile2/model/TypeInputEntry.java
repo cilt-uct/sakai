52156cb27570ab37a1abd250e18cc727487dfd99
TurRil <lightscape2001@gmail.com>
2022-03-30 17:41:50 +0200

    D2LBR-6: merging changes - continuing to a build.

--- profile2/api/src/java/org/sakaiproject/profile2/model/TypeInputEntry.java
+++ profile2/api/src/java/org/sakaiproject/profile2/model/TypeInputEntry.java
@@ -17,7 +17,7 @@ package org.sakaiproject.profile2.model;
 
 import java.io.Serializable;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import lombok.Data;
 
